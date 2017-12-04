80ecbe6df6cadd24396ed06aeb62d6f2d1b61a0f
richard2104 <richard2104a@gmail.com>
2017-12-03 23:20:54 -0800

    finished

--- site_unblock.py
+++ site_unblock.py
@@ -1,6 +1,5 @@
 import os,sys,thread,socket
 
-
 MAX_QUEUE = 20          # Max number of connection
 MAX_PKT_SIZE = 99999     # Max size of packet
 
@@ -23,19 +22,21 @@ def main():
             s.close()
         print "Fail to open socket"
         sys.exit(1)
+    except KeyboardInterrupt:
+		s.close()
+		sys.exit(1)
 
     # connect with web client 
     while 1:
         conn, client_addr = s.accept()
     
-		# thread to handle web client and end server
+		# thread to handle Web Client & End Server
 		# thread.start_new_thread(func, args, kwargs=None)
         thread.start_new_thread(proxy_thread, (conn, client_addr))
 
     s.close()
 
 def proxy_thread(conn, client_addr):
-
 	# get response from web browser
     req = conn.recv(MAX_PKT_SIZE)
     """ format of req!!!!!
@@ -43,38 +44,24 @@ GET http://www.sex.com/ HTTP/1.1
 Host: www.sex.com
 User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:54.0) Gecko/20100101 Firefox/54.0
 	""" 
-	# split first line 
-    line = req.split('\n')[0] 
-	# get url!! GET : [0], http://www.sex.com : [1]
-    url = line.split(' ')[1] 
-    http_pos = url.find("://")          # find position of ://
-    if (http_pos==-1): # if it does not exist
-        temp = url
-    else:
-        temp = url[(http_pos+3):]       # get the rest of url print temp # print the URLs!!
-    # find / in url and remove it
-
-    webserver_pos = temp.find("/")
-    if webserver_pos == -1:
-        webserver_pos = len(temp)
-    webserver = temp[:webserver_pos]
+    host = req[req.find('Host:') + 6:].split('\n')[0][:-1]
+# print host	
 
     try:
-        # create a socket to connect to the end server
+        # create a socket to connect to the End server
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # connect end server with port 80
-        s.connect((webserver, 80))
-        # put dummy behind of request
+        # connect End server with port 80
+        s.connect((host, 80))
+        # put dummy request / other website is too late
         dummyRequest = 'GET / HTTP/1.1\r\nHost: test.gilgil.net\r\n\r\n'
         req = dummyRequest + req
         # send request to end server
-        s.send(req)         
+        s.send(req)
         
+		# Receive data from end server
         while 1:
-            # receive data from end server
             data = s.recv(MAX_PKT_SIZE)
 			# print data
-            # if 404 data, ignore it
             if data.find('HTTP/1.1 404 Not Found') >= 0:
                 continue
             if (len(data) > 0):
@@ -82,11 +69,18 @@ User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:54.0) Gecko/20100101 Firefo
                 conn.send(data)
             else:
                 break
+		#time.sleep(5)
         s.close()
         conn.close()
     except socket.error, (value, message):
         s.close()
         conn.close()
         sys.exit(1)
+    except Exception as e:
+	print 'Exception from the host ' + host
+	print e
+	s.close()
+	conn.close()
 
+	
 main()
